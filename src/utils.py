e67d07870ffa414ed05de2216ce8ef29a418965f
jagdishrj37 <jagdishbugaliya67@gmail.com>
2026-04-13 00:07:50 +0530

    your message

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -17,6 +18,7 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
     
@@ -47,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
